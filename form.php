5e566c74c698b1af8cee4c83c0849603a3889039
SufardiM <114009455+SufardiM@users.noreply.github.com>
2023-03-06 14:14:49 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,4 @@
+<form name="form" action="proses.php" method="POST">
+    Nilai : <input type="text" name="angka" /> <br/>
+    <input type="submit" value="SUBMIT"/>
+</form>
---
+++ proses.php
@@ -0,0 +1,5 @@
+File Proses <hr/>
+<?php 
+$Nilai = $_POST["angka"];
+echo "Nilai Anda = ".$Nilai."<br/>";
+?>
